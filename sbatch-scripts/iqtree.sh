438490ec305bd9c8ce9c9009fee933b2ee3fcaf2
Elise Cutts <elise.m.cutts@gmail.com>
2021-01-08 13:08:09 -0500

    push

--- sbatch-scripts/iqtree.sh
+++ sbatch-scripts/iqtree.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 #SBATCH -p sched_mit_g4nier
 #SBATCH -N 1
-#SBATCH --mem=6G
-#SBATCH -J iqtree_csulf_amidohydrolase_homologs
-#SBATCH -o iqtree_csulf_amidohydrolase_homologs.out
+#SBATCH -J iqtree_rprot-aln-selected
+#SBATCH -o iqtree_rprot-aln-selected.out
 #SBATCH --mail-type=begin
 #SBATCH --mail-type=end
 #SBATCH --mail-user=ecutts@mit.edu
 
 module add engaging/iqtree/1.6.3
 
-cd ~/Amidohydrolases/tree-ncbi7001200-treesample/
-iqtree -s amidohydrolase-plusTreeSample.aln -st AA -nt AUTO -mem 6G
+cd ~/Bacteroidetes/rprot-aln-selected/
+iqtree -s concatenated_alignment.aln -q partitions.txt -st AA -nt AUTO
